08e3f170fac45509c8deacee91df57e07fac838f
bloodyt3ars <56675669+bloodyt3ars@users.noreply.github.com>
2020-07-14 23:22:46 +0300

    Небольшие правки

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from aiogram import Bot, Dispatcher, executor
-from config import BOT_TOKEN
+from config import BOT_TOKEN, admin_id
 
 from sqlighter import SQLighter
 
@@ -9,7 +9,17 @@ dp = Dispatcher(bot)
 db = SQLighter('db.db')
 
 
+async def on_shutdown(dp):
+    await bot.send_message(chat_id=admin_id, text="Бот завершил работу")
+    await bot.close()
+
+
+async def on_startup(dp):
+    await bot.send_message(chat_id=admin_id, text="Бот начал свою работу")
+
+
 if __name__ == '__main__':
     from handlers import dp
-
-    executor.start_polling(dp)
+    from admin_panel import dp
+    from callback import dp
+    executor.start_polling(dp, on_shutdown=on_shutdown, on_startup=on_startup)
